1fa18d6eee797f0d10670a38627df55bec8145bc
andyesp77-code <andyesp77@gmail.com>
2026-09-23 00:20:12 -0300

    Add files via upload
    2da entrega con ajustes solicitados por el profesor

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 En esta versión se incorpora el uso de objetos y clases.
 La clase Usuario representa a cada usuario del sistema y tiene la responsabilidad
 de guardar sus datos y definir acciones relacionadas con él, como validar la clave,
-cambiarla, blanquearla, verificar si es administrador y mostrar sus datos.
+cambiarla, blanquearla.
 
 El array usuariosRegistrados guarda instancias de Usuario, por lo que cada usuario
 se maneja como un objeto con su propio nombre y clave.
@@ -30,37 +30,38 @@ class Usuario {
     this.clave = clave;
   }
 //Operaciones que requieren usar o modificar los datos de una instancia especifica de un usuario
-//Metodos que requideren instancia de usuario
-  validarClave(claveIngresada) {
-    return this.clave === claveIngresada;
-  }
-
+//Metodos que requieren instancia de usuario
   cambiarClave(nuevaClave) {
     this.clave = nuevaClave;
-    console.log(`Clave actualizada para ${this.nombre}`);
+    console.log("Clave actualizada para " + this.nombre);
   }
 
   blanquearClave() {
     this.clave = 1111;
-    console.log(`La clave de ${this.nombre} fue blanqueada. Nueva clave: 1111`);
+    console.log("La clave de " + this.nombre + " fue blanqueada. Nueva clave: 1111");
   }
 
 
 //Metodos generales de la clase que no requieren una instancia de usuario, sino que trabaja sobre la coleccion
 //Por ese motivo las declaro como static
-  static buscarUsuario(nombre, usuarios) {
-    return usuarios.find((usuario) => usuario.nombre === nombre);
+
+static buscarUsuario(nombre, usuarios) {
+  for (const usuario of usuarios) {
+   if (usuario.nombre === nombre) {
+    return usuario;
+   }
   }
+}
 
-  static validarUsuario(nombre, clave, usuarios) {
-    const usuario = this.buscarUsuario(nombre, usuarios);
+static validarUsuario(nombre, clave, usuarios) {
+  const usuario = this.buscarUsuario(nombre, usuarios);
 
-    if (!usuario) {
-      return false;
-    }
-
-    return usuario.validarClave(clave);
+  if (!usuario) {
+    return false;
+    } else {
+    return usuario.clave === clave;
   }
+}
 
 }
 
@@ -74,18 +75,16 @@ let usuariosRegistrados = [
     new Usuario("ADMIN", 2990)
 ];
 
-
 let saldo = 100000;
 let intentos = 0;
 let login = false;
-let valor = 0;
 
 //*****Funciones del panel de administrador*****
 
 function listarUsuarios() {
     console.log("Usuarios registrados:");
     for (const usuario of usuariosRegistrados) {
-         console.log(`${usuario.nombre} - ${usuario.clave}`);
+    console.log(usuario.nombre + " - " + usuario.clave);
     }
 }
 
@@ -97,78 +96,69 @@ function blanquearClave(nombreUsuario) {
 
     if (!usuario) {
         console.log("Usuario no encontrado.");
-        return;
-    }
-
+    } else {
     usuario.blanquearClave();
-}
-
-function cambiarClaveDeUsuario(nombreUsuario, nuevaClave) { //Tambien se usa en el menu Cajero para cambiar la clave del usuario logueado
-    if (nombreUsuario === null || nuevaClave === null) {
-        console.log("Operación cancelada.");
-        return;
     }
+
+}
+//Tambien se usa en el menu Cajero para cambiar la clave del usuario logueado
+function cambiarClaveDeUsuario(nombreUsuario, nuevaClave) {
 
     nombreUsuario = nombreUsuario.trim().toUpperCase();
-   
-    if (nombreUsuario === "" || isNaN(nuevaClave)) {
-        console.log("El nombre es obligatorio y la clave debe ser un número.");
-        return;
+
+    if (isNaN(nuevaClave)) {
+        console.log("La clave debe ser un número.");
+        return false;
     }
 
     const usuario = Usuario.buscarUsuario(nombreUsuario, usuariosRegistrados);
 
     if (!usuario) {
         console.log("Usuario no encontrado.");
-        return;
+        return false;
     }
 
     usuario.cambiarClave(nuevaClave);
-    console.log("La clave de " + usuario.nombre + " fue actualizada correctamente.");
+    return true;
 }
-
 
 function eliminarUsuario(nombreUsuario) {
     nombreUsuario = nombreUsuario.trim().toUpperCase();
 
     if (nombreUsuario === "ADMIN") {
-        console.log("El usuario ADMIN no puede ser eliminado.");
-        return;
-    }
-
+    console.log("El usuario ADMIN no puede ser eliminado.");
+    } else {
     const usuario = Usuario.buscarUsuario(nombreUsuario, usuariosRegistrados);
 
     if (!usuario) {
         console.log("Usuario no encontrado.");
-        return;
+    } else {
+        // Elimina el usuario  
+        const posicionUsuario = usuariosRegistrados.indexOf(usuario);
+        usuariosRegistrados.splice(posicionUsuario, 1);
+        console.log("El usuario " + nombreUsuario + " fue eliminado del sistema.");
+        return true;
+        }
     }
-
-    const posicionUsuario = usuariosRegistrados.indexOf(usuario);
-    usuariosRegistrados.splice(posicionUsuario, 1);
-    console.log("El usuario " + nombreUsuario + " fue eliminado del sistema.");
 }
 
 function registroUsuario(nombre, clave) {
-    if (nombre === null || clave === null) {
-        console.log("Registro cancelado.");
-        return;
-    }
 
     nombre = nombre.trim().toUpperCase();
-    clave = parseInt(clave);
 
-    if (nombre.trim() === "" || isNaN(clave)) {
-        console.log("El nombre es obligatorio y la clave debe ser un número.");
-        return;
+    if (isNaN(clave)) {
+        console.log("La clave debe ser un número.");
+        return false;
     }
 
     if (Usuario.buscarUsuario(nombre, usuariosRegistrados)) {
         console.log("El usuario ya está registrado.");
-        return;
+        return false;
     }
 
     usuariosRegistrados.push(new Usuario(nombre, clave));
     console.log("Nuevo usuario registrado: " + nombre);
+    return true;
 }
 
 
@@ -191,54 +181,79 @@ function solicitarDato(mensaje) {
     return dato;
 }
 
+//Funciones para simplificar el menu administrador
+function solicitarRegistroUsuario() {
+    const nombreUsuario = solicitarDato("Ingrese el usuario");
+
+    if (nombreUsuario !== null) {
+        const claveUsuario = solicitarDato( "Ingrese la clave nueva de 4 dígitos");
+        if (claveUsuario !== null) {
+            registroUsuario(nombreUsuario, parseInt(claveUsuario));
+        }
+    }
+}
+
+function solicitarBlanqueoClave() {
+    const nombreUsuario = solicitarDato("Ingrese el nombre del usuario a blanquear");
+
+    if (nombreUsuario !== null) {
+        blanquearClave(nombreUsuario);
+    }
+}
+
+function solicitarCambioClave() {
+    const nombreUsuario = solicitarDato( "Ingrese el nombre del usuario");
+
+    if (nombreUsuario !== null) {
+            const claveNueva = solicitarDato("Ingrese la nueva clave de 4 dígitos");
+        if (claveNueva !== null) {
+            cambiarClaveDeUsuario(nombreUsuario, parseInt(claveNueva));
+        }
+    }
+}
+
+function solicitarEliminacionUsuario() {
+    const nombreUsuario = solicitarDato("Ingrese el nombre del usuario a eliminar");
+
+    if (nombreUsuario !== null) {
+        eliminarUsuario(nombreUsuario);
+    }
+}
+
+
+
 //*****Menu de opciones del panel de administrador*****/
 function menuAdmin() {
     let opcion = 0;
 
     while (opcion !== 6) {
-        opcion = parseInt(prompt("MENU ADMINISTRADOR\n1 - Listar usuarios\n2 - Nuevo usuario\n3 - Blanquear clave\n4 - Cambiar clave\n5 - Eliminar usuario\n6 - Salir"));
+        const opcionIngresada = prompt("MENU ADMINISTRADOR\n1 - Listar usuarios\n2 - Nuevo usuario\n3 - Blanquear clave\n4 - Cambiar clave\n5 - Eliminar usuario\n6 - Salir");
+            if (opcionIngresada === null) {
+            opcion = 6;
+            } else {
+            opcion = parseInt(opcionIngresada);
+            }
 
-        switch (opcion) {
+            switch (opcion) {
             case 1:
                 listarUsuarios();
                 break;
-            case 2: {
-                let nombreUsuario = solicitarDato("Ingrese el usuario");
-                if (nombreUsuario === null) break;
 
-                let claveUsuario = solicitarDato("Ingrese la clave nueva de 4 digitos");
-                if (claveUsuario === null) break;
-
-                registroUsuario(nombreUsuario, parseInt(claveUsuario));
+            case 2:
+                solicitarRegistroUsuario();
                 break;
-            }
 
-            case 3: {
-                let nombreUsuario = solicitarDato("Ingrese el nombre del usuario a blanquear");
-                if (nombreUsuario === null) break;
-
-                blanquearClave(nombreUsuario);
+            case 3:
+                solicitarBlanqueoClave();
                 break;
-            }
 
-            case 4: {
-                let nombreUsuario = solicitarDato("Ingrese el nombre del usuario");
-                if (nombreUsuario === null) break;
-
-                let claveNueva = solicitarDato("Ingrese la nueva clave de 4 digitos");
-                if (claveNueva === null) break;
-
-                cambiarClaveDeUsuario(nombreUsuario, parseInt(claveNueva));
+            case 4:
+                solicitarCambioClave();
                 break;
-            }
 
-            case 5: {
-                let nombreUsuario = solicitarDato("Ingrese el nombre del usuario a eliminar");
-                if (nombreUsuario === null) break;
-
-                eliminarUsuario(nombreUsuario);
+            case 5:
+                solicitarEliminacionUsuario();
                 break;
-            }
 
             case 6:
                 console.log("Saliendo del panel de administrador");
@@ -250,6 +265,7 @@ function menuAdmin() {
         }
     }
 }
+
 
 //*****Funciones de operacion de cajero automatico*****/
 
@@ -267,22 +283,107 @@ function depositoDinero(valor) {
     console.log("Operacion Confirmada. Su nuevo saldo es: $" + saldo);
 }
 
+//Funciones para simplificar el menu cajero
+function consultarSaldo() {
+    console.log("Su saldo es: $" + saldo);
+}
+
+function solicitarExtraccion() {
+    const datoValor = solicitarDato("¿Cuánto dinero desea retirar?");
+
+    if (datoValor !== null) {
+        const valor = parseFloat(datoValor);
+        if (isNaN(valor) || valor <= 0) {
+            console.log("Debe ingresar un importe válido mayor que cero.");
+        } else {
+            extraccionDinero(valor);
+        }
+    }
+}
+
+function solicitarDeposito() {
+    const datoValor = solicitarDato("¿Cuánto dinero desea depositar?");
+
+    if (datoValor !== null) {
+        const valor = parseFloat(datoValor);
+        if (isNaN(valor) || valor <= 0) {
+            console.log("Debe ingresar un importe válido mayor que cero.");
+        } else {
+            depositoDinero(valor);
+        }
+    }
+}
+
+function solicitarCambioClaveUsuario(nombreUsuario) {
+    const claveNueva = solicitarDato( "Ingrese su nueva clave de 4 dígitos");
+
+    if (claveNueva !== null) {
+        cambiarClaveDeUsuario(nombreUsuario, parseInt(claveNueva));
+    }
+}
+
+function menuCajero(nombreUsuario) {
+    let continuarOperando = true;
+    while  (continuarOperando) {
+        const opcionIngresada = prompt("MENU CAJERO\n" + "Ingrese una opción para operar:\n" + "Ingrese 1 - Consulta Saldo\n" + "Ingrese 2 - Extracción\n" + "Ingrese 3 - Depósito\n" + "Ingrese 4 - Cambiar Clave\n" + "Otro valor para Salir");
+        if (opcionIngresada === null) {
+            continuarOperando = false;
+        } else {
+            const opcion = parseInt(opcionIngresada);
+         switch (opcion) {
+            case 1:
+                consultarSaldo();
+                break;
+
+            case 2:
+                solicitarExtraccion();
+                break;
+
+            case 3:
+                solicitarDeposito();
+                break;
+
+            case 4:
+                solicitarCambioClaveUsuario(nombreUsuario);
+                break;
+
+            default:
+                continuarOperando = false;
+                break;
+            }
+        }
+
+        if (continuarOperando) {
+            continuarOperando = confirm(
+                "¿Desea realizar otra operación?"
+            );
+        }
+    }
+}
+
+
+
 //****ENTRADA DE DATOS PARA USUARIOS REGISTRADOS EN EL SISTEMA  *****
 let nombre = "";
-
+//intentos de login de un usuario maximo 3
 while (intentos < 3 && !login) {
-    //let nombreIngresado = prompt("Ingrese su nombre de usuario");
 
-    let nombreIngresado = solicitarDato("Ingrese el nombre del usuario");
-    if (nombreIngresado === null) break;
+    const nombreIngresado = solicitarDato("Ingrese el nombre del usuario");
+    if (nombreIngresado === null) {
+        break;
+    }
    
     nombre = nombreIngresado.trim().toUpperCase();
 
-    let claveIngresada = solicitarDato("Ingrese su clave de 4 dígitos");
+    const claveIngresada = solicitarDato("Ingrese su clave de 4 dígitos");
 
-    if (claveIngresada === null) break;
+    if (claveIngresada === null) {
+        break;
+    }
     
-    if (Usuario.validarUsuario(nombre, parseInt(claveIngresada.trim()), usuariosRegistrados)) {
+    const clave = parseInt(claveIngresada);
+
+    if (Usuario.validarUsuario(nombre,clave,usuariosRegistrados)) {
         login = true;
         console.log("Acceso concedido. Puede operar con su cuenta.");
     } else {
@@ -291,70 +392,24 @@ while (intentos < 3 && !login) {
     }
 }
 
+ // si el usuario no logra loguearse en 3 intentos, no puede acceder al sistema
 if (!login && intentos >= 3) {
     console.log("Acceso denegado. Ha superado el número máximo de intentos.");
 }
 
-const usuarioActual = Usuario.buscarUsuario(nombre, usuariosRegistrados);
+// valido si se logueo antes de buscar al usuario y mostrar el menu correspondiente
+if (login) {
+    const usuarioActual = Usuario.buscarUsuario(nombre,usuariosRegistrados);
 
-if (login && usuarioActual && usuarioActual.nombre.toUpperCase() === "ADMIN") { 
-    console.log("Hola, ADMIN. Bienvenido al panel administrativo");
-    menuAdmin();
-} else if (login) {
-    console.log("Hola, " + nombre + ". Bienvenido al Simulador de Cajero Automatico");
-
-    while (login) {
-        let opcion = parseInt(prompt("MENU CAJERO \nIngrese una opcion para operar:\nIngrese 1 - Consulta Saldo \nIngrese 2 - Extraccion \nIngrese 3 - Deposito \nIngrese 4 - Cambiar Clave \nOtro valor para Salir"));
-
-        switch (opcion) {
-            case 1:
-                console.log("Su saldo es: $" + saldo);
-                break;
-            case 2: {
-                let datoValor = solicitarDato("¿Cuánto dinero desea retirar?");
-                if (datoValor === null) break;
-                 valor = parseFloat(datoValor);
-
-                    if (isNaN(valor) || valor <= 0) {
-                    console.log("Debe ingresar un importe válido mayor que cero.");
-                break;
-                }
-
-                extraccionDinero(valor);
-                break;
-            }   
-            case 3:{
-                let datoValor = solicitarDato("¿Cuánto dinero desea depositar?");
-                if (datoValor === null) break;
-                valor = parseFloat(datoValor);
-                if (isNaN(valor) || valor <= 0) {
-                    console.log("Debe ingresar un importe válido mayor que cero.");
-                    break;
-                }
-                depositoDinero(valor);
-                break;
-            }
-            case 4:{
-                let claveNueva = solicitarDato("Ingrese su nueva clave de 4 dígitos");
-                if (claveNueva === null) break;
-                cambiarClaveDeUsuario(nombre, parseInt(claveNueva));
-                break;
-            }
-
-            default:
-                login = false;
-                break;
-        }
-
-        if (login) {
-            login = confirm("¿Desea realizar otra operacion?");
-        }
+    if (usuarioActual && usuarioActual.nombre === "ADMIN") {
+        console.log("Hola, ADMIN. Bienvenido al panel administrativo");
+        menuAdmin();
+    } else {
+        console.log("Hola, " + nombre + ". Bienvenido al Simulador de Cajero Automático");
+        menuCajero(nombre);
+        console.log("Gracias, " + nombre + " por utilizar el Simulador de Cajero Automático");
     }
-
-    console.log("Gracias, " + nombre + " por utilizar el Simulador de Cajero Automatico");
 }
-
-
 
 
 
